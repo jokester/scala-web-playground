3b6c5e52a2850e30a8323fa7a0d04eee1043edd0
Wang Guan <momocraft@gmail.com>
2019-03-14 14:09:38 +0900

    recognize (POST + _method param) as PATCH

--- server/src/main/scala/io/jokester/scala_server_playground/hello/HelloHandler.scala
+++ server/src/main/scala/io/jokester/scala_server_playground/hello/HelloHandler.scala
@@ -1,18 +1,32 @@
 package io.jokester.scala_server_playground.hello
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directive0, Directives, Route}
 import com.typesafe.scalalogging.LazyLogging
 import io.jokester.scala_server_playground.util.Catch404
 
-class HelloHandler extends LazyLogging with Catch404 {
+class HelloHandler extends LazyLogging with Catch404 with Directives {
   def route: Route = {
     (get & pathEndOrSingleSlash) {
       logger.debug("hello")
       complete(
         HttpEntity(ContentTypes.`text/plain(UTF-8)`, "hello from HelloHandler")
       )
-    } ~ catchUnmatched
+    } ~ tryPatchAlternative ~ catchUnmatched
   }
+
+  def patchRoute: Route = complete("patch!")
+
+  def patchFromX: Directive0 = {
+    post & overrideMethodWithParameter("_method") & patch
+  }
+
+  def tryPatchAlternative: Route = {
+    path("try-patch") {
+      (patch | patchFromX) {
+        patchRoute
+      }
+    }
+  }
+
 }
